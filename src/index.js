bf58c0f6e2d1c05a5870a19e2c181c8b3f2555e0
Nbelman1 <116769408+Nbelman1@users.noreply.github.com>
2026-01-06 21:33:33 -0500

    Added for loop

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ console.log("Hello world!");
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+let result = "h";
+for (let i = 0; i < 3; i++) {
+    result += "i";
+    console.log(result);
+}
+
+
